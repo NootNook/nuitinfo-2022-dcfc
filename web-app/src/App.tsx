d201f755122d248ec05628702e5459ae0cc37bc0
NootNook <67638224+NootNook@users.noreply.github.com>
2022-12-01 22:04:51 +0100

    init web app

---
+++ web-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Captcha from './Captcha';
+import Game from './Game';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+      <Game />
+    </div>
+  );
+};
+
+export default App;
